77d5915a1c5edc5d3f8c40b5c9530afcf2eb11e1
fredjuma007 <fredjuma007>
2023-05-01 18:04:43 +0200

    fixing impport of flask cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,46 +2,34 @@
 '''
     app for registering blueprint and starting flask
 '''
-from flask import Flask, jsonify
-from models import storage
-import os
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
+from models import storage
 from api.v1.views import app_views
+from os import getenv
+
 
 app = Flask(__name__)
-''' The Flask web application instance'''
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.url_map.strict_slashes = False
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
-def teardown_flask(exception):
-    ''' The Flask app/request context end event listener '''
-    # print(exception)
+def tear_down(self):
+    '''
+    close query after each session
+    '''
     storage.close()
 
 
 @app.errorhandler(404)
-def error_404(error):
+def not_found(error):
     '''
-    a handler for 404 errors that returns a
-    JSON-formatted 404 status code response
+    return JSON formatted 404 status code response
     '''
-    return (jsonify({"error": "Not found"}), 404)
-
-@app.errorhandler(400)
-def error_400(error):
-    '''Handles the 400 HTTP error code.'''
-    msg = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        msg = error.description
-    return jsonify(error=msg), 400
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    hosts = os.getenv("HBNB_API_HOST", default='0.0.0.0')
-    ports = int(os.getenv("HBNB_API_PORT", default='5000'))
-    app.run(host=hosts, port=ports, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
